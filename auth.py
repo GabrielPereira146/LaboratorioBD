c82417aae95422248d88806dcfe192d068d8b003
gil-a-a <98371042+gil-a-a@users.noreply.github.com>
2024-11-29 13:33:04 -0300

    fix: insert queries

--- auth.py
+++ auth.py
@@ -1,17 +1,22 @@
 import hashlib
 import streamlit as st
 from db import get_connection
+from datetime import datetime
 
 def validar(nome, email, senha, dt_nasc):
     if nome == "" or email == "" or senha == "" or dt_nasc == "":
         return False
     return True
 
-def registrar_usuario():
+def registrar_usuario(nome, email, senha, dt_nasc):
     nome = st.session_state.register_username
     email = st.session_state.register_email
     senha = st.session_state.register_password
     dt_nasc = st.session_state.register_dt_nasc
+
+    data_obj = datetime.strptime(dt_nasc, "%d/%m/%Y")  # Converte a string para um objeto datetime
+    data_formatada = data_obj.strftime("%Y-%m-%d")
+    print(data_formatada)
 
     if senha != st.session_state.password_confirmation:
         st.error("As senhas não coincidem. Por favor, tente novamente.")
@@ -24,12 +29,14 @@ def registrar_usuario():
     conn = get_connection()
     cursor = conn.cursor()
 
-    inp = f"INSERT INTO Usuario VALUES (9, '{nome}', '{email}', now(), 10, sha('{senha}')), '{dt_nasc}'"
+    inp = f"INSERT INTO usuario VALUES (9, '{nome}', '{email}', 21, now(), sha('{senha}'), '{data_formatada}')"
+    print(inp)
     try:
         cursor.execute(inp)
         conn.commit()
         st.success(f"Usuário {nome} registrado com sucesso!")
     except Exception as e:
+        print("erro")
         conn.rollback()
         st.error(f"Erro ao cadastrar o usuário: {e}")
     finally:
@@ -40,7 +47,7 @@ def login_user(email, password):
     conn = get_connection()
     cursor = conn.cursor()
     
-    inp = f"SELECT * FROM Usuario WHERE email = '{email}' AND senha = sha('{password}');"
+    inp = f"SELECT * FROM usuario WHERE email = '{email}' AND senha = sha('{password}');"
     cursor.execute(inp)
     result = cursor.fetchall()
     
