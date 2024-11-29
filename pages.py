a4c41d07e147e00b89d7edeba3cbb9e1be8587fe
gil-a-a <98371042+gil-a-a@users.noreply.github.com>
2024-11-29 16:09:59 -0300

    Add files via upload

--- pages.py
+++ pages.py
@@ -1,11 +1,14 @@
 from db import get_connection
 import streamlit as st
 
-
+#Selecionar uma escola e listar todas as turmas
 def show_school_class(school_id):
     conn = get_connection()
     cursor = conn.cursor()
     
+    print("School_id: ")
+    print(school_id)
+
     inp = f"""SELECT t.ID_TURMA
             FROM turma t
             JOIN escola e ON t.CO_ENTIDADE = e.CO_ENTIDADE
@@ -13,11 +16,14 @@ def show_school_class(school_id):
     cursor.execute(inp)
     result = cursor.fetchall()
     
+    print("Result")
+    print(result)
+
     cursor.close()
     conn.close()
     return result
 
-
+#Ordena as escolas por número de alunos
 def school_orderby_students():
     conn = get_connection()
     cursor = conn.cursor()
@@ -33,6 +39,23 @@ def school_orderby_students():
     conn.close()
     return result
 
+#Ordena as escolas por ordem alfabética
+def list_schools():
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    inp = """SELECT e.CO_ENTIDADE,
+                e.NO_ENTIDADE
+             FROM escola e
+             ORDER BY e.NO_ENTIDADE;"""
+    cursor.execute(inp)
+    result = cursor.fetchall()
+    
+    cursor.close()
+    conn.close()
+    return result
+
+#Listar as escolas da cidade
 def get_school_stats():
     conn = get_connection()
     cursor = conn.cursor()
@@ -69,3 +92,40 @@ def get_school_stats():
     cursor.close()
     conn.close()
     return results
+
+#Selecionar os professores e alunos de cada escola (drill-down)
+def show_teachers_students(school_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    # print("[Profs] - school_id")
+    # print(school_id)
+
+    #colocar o parâmetro e substituir no lugar de e.CO_ENTIDADE
+    inp = f"""
+        SELECT e.CO_ENTIDADE,
+                COALESCE(x.alunos, 0) AS Alunos,
+                COALESCE(y.professores, 0) AS Professores
+        FROM escola e
+        LEFT JOIN (SELECT m.CO_ENTIDADE AS entidade,
+                        m.CO_PESSOA_FISICA AS alunos
+                FROM matricula m
+                group by m.CO_ENTIDADE, m.CO_PESSOA_FISICA)
+            AS x on x.entidade = e.CO_ENTIDADE
+        LEFT JOIN (SELECT d.CO_ENTIDADE AS entidade,
+                        d.CO_PESSOA_FISICA AS professores
+                FROM docente d
+                group by d.CO_ENTIDADE, d.CO_PESSOA_FISICA)
+            AS y on y.entidade = e.CO_ENTIDADE
+        WHERE e.CO_ENTIDADE = {school_id}
+        ;
+        """
+    cursor.execute(inp)
+    result = cursor.fetchall()
+
+    # print("[Profs] - result")
+    # print(result)
+    
+    cursor.close()
+    conn.close()
+    return result
