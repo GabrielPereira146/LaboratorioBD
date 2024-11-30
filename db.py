6df09917bb8682ab789a3d10b5ef94104f47eb6b
Gabriel Pereira <gabriel.brown1112@gmail.com>
2024-11-30 12:26:42 -0300

    fix

--- db.py
+++ db.py
@@ -1,20 +1,17 @@
 import os
 import pymysql
-from dotenv import load_dotenv
-
-# Carrega variáveis de ambiente do arquivo .env
-load_dotenv()
 
 def get_connection():
+    timeout = 10
     return pymysql.connect(
-        charset=os.getenv("DB_CHARSET", "utf8mb4"),
-        connect_timeout=int(os.getenv("DB_TIMEOUT", 10)),
+        charset="utf8mb4",
+        connect_timeout=timeout,
         cursorclass=pymysql.cursors.DictCursor,
         db=os.getenv("DB_NAME"),
         host=os.getenv("DB_HOST"),
         password=os.getenv("DB_PASSWORD"),
-        read_timeout=int(os.getenv("DB_TIMEOUT", 10)),
+        read_timeout=timeout,
         port=int(os.getenv("DB_PORT")),
         user=os.getenv("DB_USER"),
-        write_timeout=int(os.getenv("DB_TIMEOUT", 10)),
+        write_timeout=timeout,
     )
